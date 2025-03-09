db71ccc631148c3f0347a8dd0f5798d373573fde
hrushikesh02-hub <hrushikeshthombare95@gmail.com>
2025-03-09 13:19:13 +0530

    Assignment_05.c

--- Assignment_05.c
+++ Assignment_05.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 struct Student{
     int id;
     char name[50];
@@ -17,7 +18,7 @@ void insertStudents(struct Student students[], int n) {
         scanf(" %[^\n]", students[i].subject);
         printf("Enter Marks: ");
         scanf("%f", &students[i].marks);
-    }
+    }printf("Students Details Successfully !\n");
 }
 void searchStudents(struct Student students[],int n){
 	int found = 0,key;
@@ -28,9 +29,9 @@ void searchStudents(struct Student students[],int n){
 			if ( students[i].id == key )
 			{
 				printf("Student found !\n");
-				printf("Roll No : %d\n",students[i].id);
+				printf("ID: %d\n",students[i].id);
 				printf("Name : %s\n",students[i].name);
-				printf("Marks : %f\n",students[i].marks);
+				printf("Marks : %.2f\n",students[i].marks);
 				found = 1 ;
 			}
 		}
@@ -38,6 +39,27 @@ void searchStudents(struct Student students[],int n){
 		{
 			printf("Student not found !\n");
 		}
+}
+void searchname(struct Student students[],int n){
+    int found = 0 ;  char key1[15];
+    printf("Enter Student Name : ");
+    scanf("%s",key1);
+    for (int i = 0 ; i < n ; i++ )
+    {
+        if(strcmp(students[i].name,key1)==0)
+        {
+        found = 1;
+        printf("Student Found !\n");
+        printf("ID : %d\n",students[i].id);
+        printf("Name : %s\n",students[i].name);
+        printf("Marks : %.2f\n",students[i].marks);
+        break;
+        }
+    }
+    if(!found)
+    {
+        printf("Student not found !\n");
+    }
 }
 
 void displayStudents(struct Student students[], int n) {
@@ -50,6 +72,8 @@ void displayStudents(struct Student students[], int n) {
 int main() {
     int n, choice, c;
     int key,found=0;
+    char key1[15];
+    int ch;
     
     printf("Enter the number of students: ");
     scanf("%d", &n);
@@ -75,8 +99,21 @@ int main() {
                 break;
 
             case 3:
-		             searchStudents(students,n);
-                break;
+                printf("1. By ID\n2. By Name\nEnter your choice : ");
+                scanf("%d",&ch);
+                switch (ch) 
+                {
+                    case 1 : 
+                        searchStudents(students,n);
+                        break;
+                    case 2 : 
+                        searchname(students,n);
+                        break;
+                    default :
+                        printf("Invalid Choice !\n");
+                }
+                
+            break;
 
             case 4:
                 printf("Exiting program...\n");
